0a87b9f4af5884680d2da0eeac647775227bc5d4
ashishchauhan01 <ashishchauhanindian@gmail.com>
2025-12-19 22:14:33 +0530

    changes

--- admin/categories.php
+++ admin/categories.php
@@ -280,9 +280,13 @@ include "header.php"; ?>
                     </div>
                 </div>
 
-                <div class=" modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                <div class="modal-footer">
+
+                    <button type="button" class="btn blue-btn">Add Category</button>
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+
+
                 </div>
             </div>
         </div>
