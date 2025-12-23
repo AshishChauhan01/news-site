80644db74fea2022544f34b0999c1d0df5ae96ee
ashishchauhan01 <ashishchauhanindian@gmail.com>
2025-12-23 16:18:37 +0530

    add post form

--- admin/posts.php
+++ admin/posts.php
@@ -1,9 +1,15 @@
 <?php
 $activePage = 'posts';
 include "header.php";
+
+$get_posts_query = "SELECT ps.*, cs.category_name, us.first_name, us.last_name FROM posts as ps 
+                    LEFT JOIN categories as cs ON ps.category_id = cs.id 
+                    LEFT JOIN users as us ON ps.author_id = us.id";
+$get_posts = mysqli_query($conn, $get_posts_query);
+
 ?>
 
-<section class="users-section section-padding min-height">
+<section class="posts-section section-padding min-height">
     <div class="container">
         <div class="section-wrapper">
             <div class="title-head">
@@ -20,8 +26,9 @@ include "header.php";
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>Post Id</th>
+                        <th></th>
                         <th>Title</th>
+                        <th>Description</th>
                         <th>Category</th>
                         <th>Date</th>
                         <th>Author</th>
@@ -31,20 +38,26 @@ include "header.php";
                 <tbody>
                     <tr>
                         <td>1.</td>
-                        <td>1</td>
+                        <td>
+                            <div class="post-thumbnail">
+                                <img src="" alt="">
+                            </div>
+                        </td>
                         <td>Lorem ipsum dolor sit, amet consectetur adipisicing.</td>
+                        <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id, obcaecati dolorum veritatis quos nobis non illum? Rem nobis optio enim perspiciatis tempora autem voluptatum pariatur fuga placeat. Vel explicabo tenetur necessitatibus, optio tempora, quos sit omnis repellat dolore qui itaque cumque eos fugit provident! Assumenda dolorum minus eligendi sequi doloribus.</td>
                         <td>technology</td>
                         <td>20aug2025</td>
                         <td>ashish</td>
-                        <td class="text-center">
+                        <td class="text-center" style="min-width:145px">
+                            <a href="" class="mx-2 view-icon">
+                                <i class="ri-eye-line text-primary"></i>
+                            </a>
                             <a href="" class="mx-2 edit-icon">
                                 <i class="fa-solid fa-pen-to-square text-secondary"></i>
                             </a>
-
                             <a href="" class="mx-2 delete-icon" onclick="return confirm('Are you sure?')">
                                 <i class="fa-solid fa-trash text-secondary"></i>
                             </a>
-
                         </td>
                     </tr>
                     <?php //else:  
